a4d4a56d99af0d8f484c5a5db8af9b5603fec316
gabe <gabrieltucker260@gmail.com>
2024-02-04 21:23:02 -0800

    model for client

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,7 +1,11 @@
-namespace HairSalon.Models
+namespace HairSalon.Models;
+
+public class Client
 {
-    public class Client
-    {
-       //
-    }
+
+    public string Name { get; set; }
+    public string Number { get; set; }
+    public int ClientId { get; set; }
+    public int StylistId { get; set; }
+    public Stylist Stylist { get; set; }
 }
